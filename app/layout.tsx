03e692c68db8bb894b0c26383239f90e5fe2caa1
seongseonko <kymgok1001@naver.com>
2024-06-29 21:03:14 +0900

    setting: tanstack query, axios 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import localFont from 'next/font/local';
+import '@/styles/globals.css';
 
 const pretendard = localFont({
   src: '../assets/fonts/PretendardVariable.woff2',
   variable: '--font-pretendard',
 });
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -12,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={`${pretendard.variable} font-pretendard`}>{children}</body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html lang="ko">
+        <body className={`${pretendard.variable} font-pretendard`}>{children}</body>
+      </html>
+    </QueryClientProvider>
   );
 }

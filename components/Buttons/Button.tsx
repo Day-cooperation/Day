a794c2bc376bd2714e7f6d11a9749b84e5c8a9b2
seongseonko <kymgok1001@naver.com>
2024-07-04 11:29:58 +0900

    feat:Button 컴포넌트font weight, size지정

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -12,20 +12,20 @@ type ButtonProps = Omit<NextButtonProps, 'size'> & {
 const CustomButton = extendVariants(NextButton, {
   variants: {
     size: {
-      xl: 'w-full',
-      lg: 'w-[291px] h-[48px]',
-      md: 'w-[150px] h-[44px]',
-      sm: 'w-[84px] h-[36px]',
+      xl: 'w-full text-base',
+      lg: 'w-[291px] h-[48px] text-base',
+      md: 'w-[150px] h-[44px] text-sm',
+      sm: 'w-[84px] h-[36px] text-sm',
     },
     variant: {
       solid:
-        'bg-blue-500 data-[hover=true]:bg-blue-600  text-white data-[pressed=true]:bg-blue-800 data-[disabled=true]:bg-slate-400',
+        'bg-blue-500 data-[hover=true]:bg-blue-600  text-white data-[pressed=true]:bg-blue-800 data-[disabled=true]:bg-slate-400 font-semibold',
       bordered:
-        'bg-white text-blue-500 data-[hover=true]:border-blue-600 data-[hover=true]:text-blue-600 data-[pressed=true]:border-blue-800 data-[pressed=true]:text-blue-800 border-blue-500',
+        'bg-white text-blue-500 data-[hover=true]:border-blue-600 data-[hover=true]:text-blue-600 data-[pressed=true]:border-blue-800 data-[pressed=true]:text-blue-800 border-blue-500 font-semibold',
     },
-    base: ['py-3 text-white data-[hover=true]:opacity-100 border-1 data-[pressed=true]:scale-100'],
+    base: ['py-3 text-white data-[hover=true]:opacity-100 border-1 data-[pressed=true]:scale-100 font-semibold'],
     isDisabled: {
-      true: 'opacity-100 text-white bg-slate-400 border-slate-400 pointer-events-auto cursor-not-allowed',
+      true: 'opacity-100 text-white font-semibold bg-slate-400 border-slate-400 pointer-events-auto cursor-not-allowed',
     },
   },
 });

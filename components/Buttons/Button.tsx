f1cf0d64246b4383f55c97ed8b0a88572a029ad9
seongseonko <kymgok1001@naver.com>
2024-07-04 14:07:27 +0900

    Refactor: 코드리뷰 반영

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -1,54 +1,48 @@
-import { Button as NextButton } from '@nextui-org/react';
+import { extendVariants, Button as NextButton } from '@nextui-org/react';
 import { ButtonProps as NextButtonProps } from '@nextui-org/react';
 
-type ButtonProps = NextButtonProps & {
+type ButtonProps = Omit<NextButtonProps, 'size'> & {
   type?: 'button' | 'submit' | 'reset';
   children?: React.ReactNode;
-  sizes?: 'xl' | 'lg' | 'md' | 'sm';
+  size?: 'xl' | 'lg' | 'md' | 'sm';
   variant?: 'solid' | 'bordered';
   disabled?: boolean;
 };
 
+const CustomButton = extendVariants(NextButton, {
+  variants: {
+    size: {
+      xl: 'w-full text-base',
+      lg: 'w-[291px] h-[48px] text-base',
+      md: 'w-[150px] h-[44px] text-sm',
+      sm: 'w-[84px] h-[36px] text-sm',
+    },
+    variant: {
+      solid:
+        'bg-blue-500 data-[hover=true]:bg-blue-600  text-white data-[pressed=true]:bg-blue-800 data-[disabled=true]:bg-slate-400 font-semibold',
+      bordered:
+        'bg-white text-blue-500 data-[hover=true]:border-blue-600 data-[hover=true]:text-blue-600 data-[pressed=true]:border-blue-800 data-[pressed=true]:text-blue-800 border-blue-500 font-semibold',
+    },
+    base: ['py-3 text-white data-[hover=true]:opacity-100 border-1 data-[pressed=true]:scale-100'],
+    isDisabled: {
+      true: 'opacity-100 text-white font-semibold bg-slate-400 border-slate-400 pointer-events-auto cursor-not-allowed',
+    },
+  },
+});
+
 export default function Button({
   type = 'button',
   children = '생성하기',
-  sizes = 'xl',
+  size = 'xl',
   variant = 'bordered',
   disabled = false,
   ...props
 }: ButtonProps) {
-  let sizeValue;
-
-  switch (sizes) {
-    case 'xl': {
-      sizeValue = 'w-full ';
-      break;
-    }
-    case 'lg': {
-      sizeValue = 'w-[291px] h-[48px]';
-      break;
-    }
-    case 'md': {
-      sizeValue = 'w-[150px] h-[44px]';
-      break;
-    }
-    case 'sm': {
-      sizeValue = 'w-[84px] h-[36px]';
-      break;
-    }
-    default: {
-      return;
-    }
-  }
-  const variantValue =
-    variant === 'solid'
-      ? 'bg-blue-500 data-[hover=true]:bg-blue-600  text-white data-[pressed=true]:bg-blue-800 data-[disabled=true]:bg-slate-400'
-      : 'bg-white text-blue-500 data-[hover=true]:border-blue-600 data-[hover=true]:text-blue-600 data-[pressed=true]:border-blue-800 data-[pressed=true]:text-blue-800 data-[disabled=true]:text-slate-400 data-[disabled=true]:border-slate-400 border-blue-500';
   return (
-    <NextButton
+    <CustomButton
       type={type}
       variant={variant}
-      className={`${sizeValue} py-3 text-white data-[hover=true]:!opacity-100 ${variantValue} data-[disabled=true]:opacity-100 border-1 data-[pressed=true]:scale-100`}
+      size={size}
       radius='md'
       disableRipple
       disableAnimation
@@ -56,6 +50,6 @@ export default function Button({
       {...props}
     >
       {children}
-    </NextButton>
+    </CustomButton>
   );
 }

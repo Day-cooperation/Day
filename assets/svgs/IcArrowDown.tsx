28a786206c6c818f523aa7c832196862edba84b8
seongseonko <kymgok1001@naver.com>
2024-07-05 16:05:44 +0900

    feat:svg 추가

---
+++ assets/svgs/IcArrowDown.tsx
@@ -0,0 +1,7 @@
+import React, { SVGProps } from 'react';
+
+export const IcArrowDown = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' {...props}>
+    <path d='M7.5 10L12 14.5L16.5 10' stroke='#334155' strokeWidth='1.8' strokeLinecap='round' strokeLinejoin='round' />
+  </svg>
+);

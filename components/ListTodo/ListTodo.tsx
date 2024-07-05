25b4d86a5fc620d88c32580dd855f26f5c81bab7
seongseonko <kymgok1001@naver.com>
2024-07-05 16:13:22 +0900

    feat:CardGoal 컴포넌트 구현

--- components/ListTodo/ListTodo.tsx
+++ components/ListTodo/ListTodo.tsx
@@ -10,11 +10,14 @@ type ListTodoButtons = 'file' | 'link' | 'note write' | 'done' | 'note' | 'edit'
 type ListTodoProps = {
   todos: Todo[];
   onButtonClick: (buttonType: ListTodoButtons, id: number) => void;
+  goalHidden?: boolean;
+  viewLength?: number;
 };
 
-export default function ListTodo({ todos, onButtonClick }: ListTodoProps) {
+export default function ListTodo({ todos, goalHidden = false, viewLength = 0, onButtonClick }: ListTodoProps) {
   const [openPopupTodoId, setOpenPopupTodoId] = useState<number | null>(null);
   const popoverRef = useRef<HTMLDivElement | null>(null);
+  const todoList = viewLength ? todos.slice(0, viewLength) : todos.slice(0);
   //최신순으로 정렬
   todos.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
   const handleClick = (buttonType: ListTodoButtons, id: number) => {
@@ -32,24 +35,29 @@ export default function ListTodo({ todos, onButtonClick }: ListTodoProps) {
   }, [openPopupTodoId]);
 
   return (
-    <div className='min-w-max'>
-      <div className='flex flex-col justify-center items-start gap-5 py-3.5 px-5'>
-        {todos.map((item) => {
+    <div className='mt-3'>
+      <div className='flex flex-col justify-center items-start gap-[10px]'>
+        {todoList.map((item) => {
           return (
             <Checkbox
               classNames={{
-                base: ['flex items-center w-full max-w-full group p-0'],
-                label: ['max-w-full w-full '],
+                base: ['flex max-w-full w-full justify-between items-start group p-0 m-0'],
+                label: ['w-full'],
                 wrapper: ['w-[18px] h-[18px]'],
               }}
               defaultSelected={item.done}
               onChange={() => handleClick('done', item.id)}
               key={item.id}
             >
-              <div className='flex justify-between items-stretch'>
-                <div className='flex flex-col items-start justify-center'>
-                  <span className='group-data-[selected=true]:line-through text-sm text-slate-800'>{item.title}</span>
-                  {item.goal && (
+              <div className='flex justify-between items-stretch truncate'>
+                <div
+                  data-goalhidden={goalHidden}
+                  className='data-[goalHidden=false]:flex-col data-[goalHidden=false]:items-start flex items-center justify-start truncate'
+                >
+                  <span className='group-data-[selected=true]:line-through text-sm text-slate-800 py-0.5'>
+                    {item.title}
+                  </span>
+                  {item.goal && !goalHidden && (
                     <span className='flex justify-center items-center gap-1.5 !no-underline text-sm text-slate-700'>
                       <Goal className='w-6 h-6 ' />
                       {item.goal.title}

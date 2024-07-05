a3a64d169bc36896b2a7def27c53b6724f7bab79
seongseonko <kymgok1001@naver.com>
2024-07-05 16:06:32 +0900

    feat:Plus Icon svg 변경

--- assets/svgs/Plus.tsx
+++ assets/svgs/Plus.tsx
@@ -1,8 +1,12 @@
 import React, { SVGProps } from 'react';
 
-export const Plus = (props: SVGProps<SVGSVGElement>) => (
+type PlusProps = SVGProps<SVGSVGElement> & {
+  strokeColor?: string;
+};
+
+export const Plus = ({ strokeColor = 'white', ...props }: PlusProps) => (
   <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' {...props}>
-    <path d='M5 12H18.5' stroke='white' strokeWidth='1.8' strokeLinecap='round' />
-    <path d='M11.75 18.75V5.25' stroke='white' strokeWidth='1.8' strokeLinecap='round' />
+    <path d='M5 12H18.5' stroke={strokeColor} strokeWidth='1.8' strokeLinecap='round' />
+    <path d='M11.75 18.75V5.25' stroke={strokeColor} strokeWidth='1.8' strokeLinecap='round' />
   </svg>
 );

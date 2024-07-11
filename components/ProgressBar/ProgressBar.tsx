fb4d331a092696538fffbbe4d3e1294ed73c71fd
chlangus <angusangus11@gmail.com>
2024-07-11 18:43:25 +0900

    feat: noteList 페이지 구현

--- components/ProgressBar/ProgressBar.tsx
+++ components/ProgressBar/ProgressBar.tsx
@@ -8,7 +8,7 @@ export default function ProgressBar({ value }: ProgressBarProps) {
   return (
     <div className='flex items-center gap-2 bg-white rounded-[13px] px-[9px]'>
       <Progress aria-label='progress' size='sm' value={value} classNames={{ indicator: ['bg-slate-900'] }} />
-      <span className='text-xs font-semibold text-slate-900'>{`${Number(value.toFixed(1))}%`}</span>
+      <span className='text-xs font-semibold text-slate-900'>{Number(Math.round(value))}</span>
     </div>
   );
 }

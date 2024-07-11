2ffe7781b741e56d5f11b8fdd4df1ee20ff4c7ea
seongseonko <kymgok1001@naver.com>
2024-07-11 20:11:02 +0900

    feat: todolist 페이지 구현

--- app/(pages)/todolist/page.tsx
+++ app/(pages)/todolist/page.tsx
@@ -1,20 +1,48 @@
 'use client';
 
-import { getRequest, patchRequest } from '@/api/api';
+import { deleteRequest, getRequest, patchRequest } from '@/api/api';
 import { Plus } from '@/assets/svgs';
 import Filter from '@/components/Filter/Filter';
 import ListTodo from '@/components/ListTodo/ListTodo';
+import Modal from '@/components/Modal/Modal';
+import NoteRead from '@/components/Note/NoteRead';
+import ConfirmPopup from '@/components/Popup/ConfirmPopup';
 import { ListTodoButtons, Todo } from '@/types/Todo';
+import { useDisclosure } from '@nextui-org/react';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useRef, useState } from 'react';
 
 export default function TodoList() {
   const queryClient = useQueryClient();
   const [todoState, setTodoState] = useState('All');
-  const { data: todoList, isPending } = useQuery({
+  const [modalType, setModalType] = useState<'create' | 'edit'>('create');
+  const [todo, setTodo] = useState<Todo>();
+  const [confirm, setConfirm] = useState({ message: '', setDeleteId: 0 });
+  const confirmRef = useRef<HTMLDialogElement>(null);
+  const noteRef = useRef<HTMLDialogElement>(null);
+  const router = useRouter();
+  const { data: todoList, isLoading } = useQuery({
     queryKey: ['get-todos'],
     queryFn: () => getRequest({ url: 'todos' }),
   });
+  const { data: goalList } = useQuery({
+    queryKey: ['goalList'],
+    queryFn: () => getRequest({ url: 'goals' }),
+  });
+  const {
+    data: noteData,
+    mutate: noteMutate,
+    isPending,
+  } = useMutation({
+    mutationKey: ['get-note'],
+    mutationFn: (id) => getRequest({ url: `notes/${id}` }),
+    onSuccess: () => {
+      if (!noteRef.current) return;
+      noteRef.current.showModal();
+    },
+  });
+
   const mutation = useMutation({
     mutationFn: ({ id, done }: { id: number; done: boolean }) =>
       patchRequest({ url: `todos/${id}`, data: { done: done } }),
@@ -23,11 +51,22 @@ export default function TodoList() {
       queryClient.invalidateQueries({ queryKey: ['get-todos'] });
     },
   });
-  const title = `${todoState === 'all' ? '모든 할 일' : todoState === 'To do' ? '할 일' : '완료한 일'}(${todoList?.data.todos.length || 0})`;
+
+  const { mutate: deleteMutate } = useMutation({
+    mutationKey: ['delete-todo'],
+    mutationFn: (id: number) => deleteRequest({ url: `todos/${id}` }),
+    onSuccess: () => {
+      setConfirm({ message: '', setDeleteId: 0 });
+      queryClient.invalidateQueries({ queryKey: ['get-todos'] });
+    },
+  });
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
   const todos =
     todoState === 'All'
       ? todoList?.data.todos
       : todoList?.data.todos.filter((item: Todo) => item.done === (todoState === 'To do' ? false : true));
+  const pageTitle = `${todoState === 'All' ? '모든 할 일' : todoState === 'To do' ? '할 일' : '완료한 일'}(${todos?.length || 0})`;
 
   const handleTodoTypeClick = (buttonType: ListTodoButtons, id: number) => {
     if (buttonType === 'done') {
@@ -35,27 +74,72 @@ export default function TodoList() {
       mutation.mutate({ id, done: !todo.done });
       return;
     }
+    if (buttonType === 'edit' || buttonType === 'file' || buttonType === 'link') {
+      setModalType('edit');
+      setTodo(todos.find((item: Todo) => id === item.id));
+      onOpen();
+    }
+
+    if (buttonType === 'delete') {
+      if (!confirmRef.current) return;
+      setConfirm({ message: '정말로 삭제하시겠어요?', setDeleteId: id });
+      confirmRef.current.showModal();
+    }
+    if (buttonType === 'note write') {
+      router.push('note');
+    }
+
+    if (buttonType === 'note read') {
+      const noteId = todos.find((item: Todo) => item.id === id).noteId;
+      noteMutate(noteId);
+    }
+  };
+
+  const handleDeleteConfirmClick = (answer: 'ok' | 'cancel') => {
+    if (answer === 'ok') {
+      deleteMutate(confirm.setDeleteId);
+    }
   };
   const handleFilterClick = (category: 'All' | 'To do' | 'Done') => {
     setTodoState(category);
   };
 
-  if (isPending) <div>loading...</div>;
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
   return (
-    <div className='w-full flex justify-center bg-slate-100'>
-      <div className='w-full mx-auto max-w-[792px] flex flex-col gap-4 '>
-        <div className='flex justify-between mt-6'>
-          <h1 className='text-lg font-semibold text-slate-900'>{title}</h1>
-          <button className='flex gap-1 items-center justify-center text-sm font-semibold text-blue-500'>
-            <Plus strokeColor='currentColor' className='w-6 h-6' />
-            할일 추가
-          </button>
-        </div>
-        <div className='flex flex-col gap-4 bg-white w-full min-h-[988px] p-6 rounded-xl mb-6'>
-          <Filter handleClick={handleFilterClick} />
-          <ListTodo todos={todos} showGoal={false} onButtonClick={handleTodoTypeClick} />
+    <>
+      <ConfirmPopup
+        type='popup'
+        dialogRef={confirmRef}
+        confirmText={confirm.message}
+        onConfirmClick={handleDeleteConfirmClick}
+        confirm
+      />
+      <NoteRead dialogRef={noteRef} data={noteData?.data} />
+      <Modal modalType={modalType} isOpen={isOpen} items={todo} onClose={onClose} goalList={goalList?.data.goals} />
+      <div className='w-full flex justify-center bg-slate-100'>
+        <div className='w-full mx-auto max-w-[792px] flex flex-col gap-4 '>
+          <div className='flex justify-between mt-6'>
+            <h1 className='text-lg font-semibold text-slate-900'>{pageTitle}</h1>
+            <button
+              className='flex gap-1 items-center justify-center text-sm font-semibold text-blue-500'
+              onClick={() => {
+                onOpen();
+                setModalType('create');
+              }}
+            >
+              <Plus strokeColor='currentColor' className='w-6 h-6' />
+              할일 추가
+            </button>
+          </div>
+          <div className='flex flex-col gap-4 bg-white w-full min-h-[988px] p-6 rounded-xl mb-6'>
+            <Filter handleClick={handleFilterClick} />
+            <ListTodo todos={todos} showGoal={true} onButtonClick={handleTodoTypeClick} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }

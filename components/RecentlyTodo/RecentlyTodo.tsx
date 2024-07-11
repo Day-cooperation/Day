39b543e943fb2354d69fda4c1bfbfd5a615de147
chlangus <angusangus11@gmail.com>
2024-07-11 11:52:38 +0900

    feat: RecentlyTodo 컴포넌트 대시보드와 연결

--- components/RecentlyTodo/RecentlyTodo.tsx
+++ components/RecentlyTodo/RecentlyTodo.tsx
@@ -1,26 +1,83 @@
+'use client';
+
 import { ArrowRight, Rectangles } from '@/assets/svgs';
 import ListTodo from '../ListTodo/ListTodo';
-import { Todo } from '@/types/Todo';
+import { ListTodoButtons, Todo } from '@/types/Todo';
+import { deleteRequest, getRequest, patchRequest } from '@/api/api';
+import CardGoal from '@/components/CardGoal/CardGoal';
+import { QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-export default function RecentlyTodo({ todoList }: { todoList: Todo[] }) {
+import Link from 'next/link';
+import { useState } from 'react';
+import Modal from '../Modal/Modal';
+import { Goal } from '@/types/Goal';
+
+export default function RecentlyTodo({ goalList }: { goalList: Goal[] }) {
+  const queryClient = useQueryClient();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['todoList'],
+    queryFn: () => getRequest({ url: 'todos' }),
+  });
+  const { mutate: updateTodoMutate } = useMutation({
+    mutationFn: ({ path, data }: { path: string; data: Todo }) => patchRequest({ url: `todos/${path}`, data }),
+    // 성공했을때 querykey 초기화 시켜줘야하나??
+    // onSuccess: () => queryClient.invalidateQueries({ queryKey: ['todoList'] }),
+  });
+  const { mutate: deleteTodoMutate, isPending } = useMutation({
+    mutationFn: ({ path }: { path: string }) => deleteRequest({ url: `todos/${path}` }),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['todoList'] }),
+  });
+
+  const handleButtonClick = (type: ListTodoButtons, id: number) => {
+    const selecteItem = data?.data.todos.filter((todo: Todo) => todo.id === id)[0];
+    if (type === 'done') {
+      updateTodoMutate({ path: String(selecteItem.id), data: { ...selecteItem, done: !selecteItem.done } });
+    }
+    if (type === 'delete') {
+      deleteTodoMutate({ path: String(selecteItem.id) });
+    }
+    if (type === 'edit') {
+      setIsModalOpen(!isModalOpen);
+    }
+  };
+
+  if (isLoading || isPending) return <h2>Loading...</h2>;
+
   return (
-    <div className='relative w-full max-w-[588px] h-[250px] bg-white rounded-xl border-1 border-slate-100 px-6 py-4 flex flex-col gap-4'>
-      <div className='flex justify-between'>
-        <div className='flex items-center justify-center gap-2'>
-          <div className='h-10 w-10 bg-blue-500 rounded-[15px] flex items-center justify-center'>
-            <Rectangles />
+    <>
+      <Modal
+        onClose={() => setIsModalOpen(!isModalOpen)}
+        isOpen={isModalOpen}
+        modalType='edit'
+        goalList={goalList}
+        modalTodoState
+      />
+      <div className='relative w-full md:max-w-[588px] h-[250px] bg-white rounded-xl border-1 border-slate-100 px-6 py-4 flex flex-col gap-4'>
+        <div className='flex justify-between'>
+          <div className='flex items-center justify-center gap-2'>
+            <div className='h-10 w-10 bg-blue-500 rounded-[15px] flex items-center justify-center'>
+              <Rectangles />
+            </div>
+            <h2 className='text-slate-800 text-lg font-semibold'>최근 등록한 할 일</h2>
           </div>
-          <h2 className='text-slate-800 text-lg font-semibold'>최근 등록한 할 일</h2>
+          <Link href='/todolist' className='flex justify-center items-center'>
+            <span className='text-sm outline-none'>모두 보기</span>
+            <ArrowRight />
+          </Link>
         </div>
-        <button className='flex justify-center items-center'>
-          <span className='text-sm outline-none'>모두 보기</span>
-          <ArrowRight />
-        </button>
+        {data?.data.todos ? (
+          <div className='h-[154px] overflow-y-auto pt-1'>
+            <ListTodo showGoal todos={data?.data.todos} onButtonClick={handleButtonClick}></ListTodo>
+          </div>
+        ) : (
+          <div className='h-[154px] flex items-center justify-center'>
+            <p className='text-sm text-slate-500'>최근 등록한 할 일이 없어요</p>
+          </div>
+        )}
+        <div className='absolute bottom-4 h-[51px] w-[calc(100%-48PX)] bg-gradient-to-b from-white/0 to-white/100 pointer-events-none' />
       </div>
-      <div className='w-full h-[154px] overflow-y-auto pt-1'>
-        <ListTodo showGoal todos={todoList} onButtonClick={(type, id) => {}}></ListTodo>
-      </div>
-      <div className='absolute bottom-4 h-[51px] w-[calc(100%-48PX)] bg-gradient-to-b from-white/0 to-white/100 pointer-events-none' />
-    </div>
+    </>
   );
 }

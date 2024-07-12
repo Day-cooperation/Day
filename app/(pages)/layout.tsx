783f2a1026712d7a2fdef1e4afbf7bb9f25a89c7
chlangus <angusangus11@gmail.com>
2024-07-12 10:27:33 +0900

    fix: prettier 적용

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,9 +3,9 @@ import '@/styles/globals.css';
 import SideMenu from '@/components/SideMenu/SideMenu';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-      <div className={`bg-slate-100 min-h-screen h-full flex flex-col md:flex-row `}>
-        <SideMenu />
-        <div className='lg:ml-[331px] w-full'>{children}</div>
-      </div>
+    <div className={`bg-slate-100 min-h-screen h-full flex flex-col md:flex-row `}>
+      <SideMenu />
+      <div className='lg:ml-[331px] w-full'>{children}</div>
+    </div>
   );
 }

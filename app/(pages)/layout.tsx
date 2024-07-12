61a46c349937a9c3cd05a76c5c8d075c2e251fcf
chlangus <angusangus11@gmail.com>
2024-07-12 10:25:58 +0900

    fix: provider 제거

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,11 +3,9 @@ import '@/styles/globals.css';
 import SideMenu from '@/components/SideMenu/SideMenu';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Providers>
       <div className={`bg-slate-100 min-h-screen h-full flex flex-col md:flex-row `}>
         <SideMenu />
         <div className='lg:ml-[331px] w-full'>{children}</div>
       </div>
-    </Providers>
   );
 }

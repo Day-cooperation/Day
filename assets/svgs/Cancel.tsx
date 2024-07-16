b05dbd39e6e4efb1e81668b9b8e201bce8a735e8
seongseonko <kymgok1001@naver.com>
2024-07-16 21:15:59 +0900

    file: svg추가

--- assets/svgs/Cancel.tsx
+++ assets/svgs/Cancel.tsx
@@ -1,7 +1,12 @@
 import React, { SVGProps } from 'react';
-export const Cancel = (props: SVGProps<SVGSVGElement>) => (
+
+type CancelProps = SVGProps<SVGSVGElement> & {
+  strokeColor?: string;
+};
+
+export const Cancel = ({ strokeColor = '#64748b', ...props }: CancelProps) => (
   <svg xmlns='http://www.w3.org/2000/svg' width='16' height='15' viewBox='0 0 16 15' fill='none' {...props}>
-    <path d='M1.5 1L14.5 14' stroke='#64748B' strokeWidth='1.8' strokeLinecap='round' />
-    <path d='M14.5 1L1.5 14' stroke='#64748B' strokeWidth='1.8' strokeLinecap='round' />
+    <path d='M1.5 1L14.5 14' stroke={strokeColor} strokeWidth='1.8' strokeLinecap='round' />
+    <path d='M14.5 1L1.5 14' stroke={strokeColor} strokeWidth='1.8' strokeLinecap='round' />
   </svg>
 );

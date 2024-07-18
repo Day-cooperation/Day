543602cc5b01e2546b863b632e7a856d69bce88f
chlangus <angusangus11@gmail.com>
2024-07-18 09:33:07 +0900

    fix: prettier 적용

--- api/axios.ts
+++ api/axios.ts
@@ -45,8 +45,7 @@ instance.interceptors.response.use(
     console.log('error.response?.message: ', originalRequest);
 
     const refreshToken = Cookies.get(REFRESH_TOKEN);
-    if (!refreshToken && (window.location.pathname !== '/signin' ||'/signup'))
-      window.location.replace('/signin');
+    if (!refreshToken && (window.location.pathname !== '/signin' || '/signup')) window.location.replace('/signin');
     // 액세스 토큰 만료 시
     if (refreshToken && error.response?.status === 401) {
       try {

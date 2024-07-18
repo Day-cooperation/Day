8c3621087d68f4835662874f1cc1f59b816f57b9
seongseonko <kymgok1001@naver.com>
2024-07-18 23:14:02 +0900

    feat: layout 동적으로 pathName으로 스타일링

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,11 +1,16 @@
 'use client';
 import '@/styles/globals.css';
 import SideMenu from '@/components/SideMenu/SideMenu';
+import { usePathname } from 'next/navigation';
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathName = usePathname().slice(-5);
   return (
-    <div className={`bg-slate-100 min-h-screen flex flex-col md:flex-row `}>
+    <div
+      data-path={pathName === 'write'}
+      className={`bg-slate-100 data-[path=true]:bg-white min-h-screen flex flex-col md:flex-row `}
+    >
       <SideMenu />
-      <div className='lg:ml-[331px] w-full'>{children}</div>
+      <div className='w-full'>{children}</div>
     </div>
   );
 }

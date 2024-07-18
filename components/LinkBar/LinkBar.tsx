af074c1fc53719f765f391a3a0d944abdde65daf
seongseonko <kymgok1001@naver.com>
2024-07-18 23:36:06 +0900

    feat: LinkBar 수정 & 스타일링

--- components/LinkBar/LinkBar.tsx
+++ components/LinkBar/LinkBar.tsx
@@ -3,27 +3,31 @@ import { useEmbedingUrlStore } from '@/stores/useEmbedingUrlStore';
 
 type LinkBarProps = {
   linkUrl: string;
+  embededOpen: (url: string) => void;
+  cancelView?: boolean;
 };
 
-export default function LinkBar({ linkUrl }: LinkBarProps) {
+export default function LinkBar({ linkUrl, embededOpen, cancelView }: LinkBarProps) {
   const { clearUrl } = useEmbedingUrlStore();
   return (
     <div className='flex items-center justify-between bg-blue-50 rounded-[28px] mb-4'>
-      <div className='flex items-center gap-[5px]'>
-        <button type='button' className='py-1 pl-1.5'>
+      <div className='flex items-center gap-[5px] overflow-hidden'>
+        <button type='button' className='py-1 pl-1.5' onClick={() => embededOpen(linkUrl)}>
           <Embeded className='w-6 h-6' />
         </button>
-        <p className='text-slate-800'>{linkUrl}</p>
+        <p className='text-slate-800 truncate'>{linkUrl}</p>
       </div>
-      <div className='py-1 pr-1.5'>
-        <button
-          type='button'
-          className='flex items-center justify-center rounded-full bg-slate-500 w-[18px] h-[18px] p-[3px]'
-          onClick={clearUrl}
-        >
-          <Cancel strokeColor={'#F8FAFC'} />
-        </button>
-      </div>
+      {cancelView && (
+        <div className='py-1 pr-1.5 shrink-0'>
+          <button
+            type='button'
+            className='flex items-center justify-center rounded-full bg-slate-500 w-[18px] h-[18px] p-[3px]'
+            onClick={clearUrl}
+          >
+            <Cancel strokeColor={'#F8FAFC'} />
+          </button>
+        </div>
+      )}
     </div>
   );
 }

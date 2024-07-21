5733b0d70a8512b141a1b4de49e7c4336a563464
chlangus <angusangus11@gmail.com>
2024-07-21 23:15:32 +0900

    fix: auth 페이지 스크롤 제거

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,8 +8,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className='w-full h-screen bg-white'>
-      <div className='mx-auto max-w-[640px] mt-[120px]'>
+    <main className='-mt-24 fixed inset-0 h-screen p-4 bg-white flex flex-col justify-center items-center'>
+      <div className='max-w-[640px] w-full'>
         <div className='flex justify-center py-[20.34px] px-[13.18px] mb-10'>
           <Logo className='w-[244.101px] h-[48.314px] shrink-0' />
         </div>

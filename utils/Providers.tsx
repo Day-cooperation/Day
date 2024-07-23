517aa439e4be0a309a94ff0819e620fd49b4f3ad
seongseonko <kymgok1001@naver.com>
2024-07-23 14:10:26 +0900

    feat: Nprogress 설치및 적용

--- utils/Providers.tsx
+++ utils/Providers.tsx
@@ -3,14 +3,18 @@
 import { NextUIProvider } from '@nextui-org/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NextUIProvider>{children}</NextUIProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <>
+      <ProgressBar height='4px' color='#3b82f6' startPosition={0.3} options={{ showSpinner: false }} shallowRouting />
+      <QueryClientProvider client={queryClient}>
+        <NextUIProvider>{children}</NextUIProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
   );
 }

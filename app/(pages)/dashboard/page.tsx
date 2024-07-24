d7f4dd6afc5ace774c75f928567ec1248c1870e9
chlangus <angusangus11@gmail.com>
2024-07-24 14:04:29 +0900

    fix: 주석 삭제

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -13,11 +13,8 @@ export default function Dashboard() {
     <div className='flex flex-col p-4 md:p-6 items-stretch max-w-[1200px] md:gap-3'>
       <h1 className='text-left max-w-full hidden md:block text-slate-900 text-lg font-semibold'>대시보드</h1>
       <main className=' flex flex-col md:gap-6 gap-4'>
-        <div className='flex flex-col md:flex-row md:gap-6 gap-4'>
-          <div className='w-full'>
-            <RecentlyTodo />
-          </div>
-          <div className=' h-[250px] w-full md:max-w-[588px] bg-blue-500 rounded-xl'></div>
+        <div className='w-full'>
+          <RecentlyTodo />
         </div>
         <div className='w-full max-w-[1200px] bg-white rounded-xl py-4 px-6 min-h-[calc(100vh-612px)] md:min-h-[calc(100vh-362px)]'>
           <div className='flex justify-start gap-[6px] items-center mb-4'>

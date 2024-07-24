992990406426a6823ebab985d9104be732b443b0
chlangus <angusangus11@gmail.com>
2024-07-24 10:51:48 +0900

    fix: prettier 적용

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -43,7 +43,6 @@ export default function Signup() {
       }
     },
   });
-
   const onSubmit: SubmitHandler<SignupInput> = (data, e) => {
     e?.preventDefault();
     delete data.passwordConfirm;

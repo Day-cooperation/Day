596f7f85e4fb3dcc4ce690ccd7e5afb057f7e25e
seongseonko <kymgok1001@naver.com>
2024-07-29 15:46:28 +0900

    fix:any로 되어있는 아규먼트 수정

--- utils/extractTextFromHtml.ts
+++ utils/extractTextFromHtml.ts
@@ -1,4 +1,4 @@
-export const extractTextFromHtml = (content: any) => {
+export const extractTextFromHtml = (content: string) => {
   const tempDiv = document.createElement('div');
   tempDiv.innerHTML = content;
   const textContent = tempDiv.textContent || '';

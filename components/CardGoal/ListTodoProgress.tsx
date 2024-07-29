d561179f51e708bf45b2954481812ffb376f506d
chlangus <angusangus11@gmail.com>
2024-07-29 13:47:37 +0900

    fix: 모바일에서 cardGoal 레이아웃 변경

--- components/CardGoal/ListTodoProgress.tsx
+++ components/CardGoal/ListTodoProgress.tsx
@@ -19,11 +19,18 @@ export default function ListTodoProgress({
   return (
     <div className='flex flex-col gap-4'>
       <span className='text-sm font-semibold text-slate-800'>{subject}</span>
-      {itemList?.length !== 0 ? (
-        <ListTodo todos={itemList} showGoal={false} displayTodoCount={displayTodoCount} onButtonClick={onUpdateList} />
-      ) : (
-        <span className='flex -mt-0.5 justify-center items-center h-[152px] text-xs text-slate-500'>{textValue}</span>
-      )}
+      <div className='h-full min-h-20 md:min-h-[152px] '>
+        {itemList?.length !== 0 ? (
+          <ListTodo
+            todos={itemList}
+            showGoal={false}
+            displayTodoCount={displayTodoCount}
+            onButtonClick={onUpdateList}
+          />
+        ) : (
+          <div className='flex -mt-0.5 justify-center h-full items-center text-xs text-slate-500'>{textValue}</div>
+        )}
+      </div>
     </div>
   );
 }

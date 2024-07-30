04ee86f293de5c50f0468bdafa3ecfdacad9f00e
chlangus <angusangus11@gmail.com>
2024-07-30 18:32:34 +0900

    fix: localStorage에 token 저장

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -11,24 +11,33 @@ export const axiosAuth = axios.create({
   baseURL: BASE_URL,
 });
 
-let cachedSession: any = null;
-
 const getCachedSession = async () => {
   // 저장된 세션이 없거나 세션 만료시간 지나면 getSession 호출
-  if (cachedSession) {
-    if (new Date().getTime() > (cachedSession.user.accessTokenExpieryTime as number))
-      cachedSession = await getSession();
+  let session: any;
+  const accessToken = localStorage.getItem('slid_accessToken');
+  const expieryTime = Number(localStorage.getItem('slid_expieryTime'));
+  if (accessToken && expieryTime) {
+    if (new Date().getTime() > expieryTime) {
+      session = await getSession();
+      localStorage.setItem('slid_accessToken', session?.user.accessToken);
+      localStorage.setItem('slid_expieryTime', session?.user.accessTokenExpieryTime);
+      return session?.user.accessToken;
+    }
   } else {
-    cachedSession = await getSession();
+    session = await getSession();
+    localStorage.setItem('slid_accessToken', session?.user.accessToken);
+    localStorage.setItem('slid_expieryTime', session?.user.accessTokenExpieryTime);
+    return session?.user.accessToken;
   }
-  return cachedSession;
+
+  return accessToken;
 };
 
 axiosAuth.interceptors.request.use(
   async (config) => {
-    const session = await getCachedSession();
-    if (session && !config.headers['Authorization']) {
-      config.headers['Authorization'] = `Bearer ${session?.user?.accessToken}`;
+    const accessToken = await getCachedSession();
+    if (accessToken && !config.headers['Authorization']) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
     }
     return config;
   },

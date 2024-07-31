25b82dbc5dab784e32b07601ecacf26e1b848499
seongseonko <kymgok1001@naver.com>
2024-07-31 18:39:52 +0900

    feat:파비콘 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import Providers from '@/utils/Providers';
 import localFont from 'next/font/local';
 import '@/styles/globals.css';
+import { Metadata } from 'next';
+import Head from 'next/head';
 
 const pretendard = localFont({
   src: '../assets/fonts/PretendardVariable.woff2',
   variable: '--font-pretendard',
 });
+
+export const metadata: Metadata = {
+  title: 'Day',
+};
 
 export default function RootLayout({
   children,
@@ -14,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <body className={`${pretendard.variable} font-pretendard bg-slate-100`}>
         <Providers>{children}</Providers>
       </body>

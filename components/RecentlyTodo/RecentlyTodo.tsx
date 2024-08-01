10f3183ddaca20fb60ca6cf3ab8ce33b9a1f73ea
chlangus <angusangus11@gmail.com>
2024-08-01 16:16:29 +0900

    fix: 로고 왼쪽으로 옮김

--- components/RecentlyTodo/RecentlyTodo.tsx
+++ components/RecentlyTodo/RecentlyTodo.tsx
@@ -37,7 +37,7 @@ export default function RecentlyTodo() {
       />
       <NoteRead dialogRef={noteRef} data={noteData} />
       <Modal onClose={onClose} isOpen={isOpen} modalType={modalType} items={todo} />
-      <div className='relative w-full h-[250px] bg-white rounded-xl border-1 border-slate-100 px-6 py-4 flex flex-col gap-4 max-w-[1200px] flex flex-col'>
+      <div className='relative w-full h-[250px] bg-white rounded-xl border-1 border-slate-100 px-6 py-4 flex flex-col gap-4 max-w-[1200px]'>
         {isLoading ? (
           <Spinner className='absolute top-[calc(50%-16px)] left-[calc(50%-16px)]' />
         ) : (

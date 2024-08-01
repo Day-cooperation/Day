002aec41dfaf12ce806167d38a7fd6be87f9a124
chlangus <angusangus11@gmail.com>
2024-08-02 00:26:53 +0900

    fix: 랜딩 페이지 마지막 문구 애니메이션 변경

--- components/Landing/ServiceStart.tsx
+++ components/Landing/ServiceStart.tsx
@@ -1,25 +1,45 @@
-import { useScroll, useTransform, motion, useMotionValueEvent } from 'framer-motion';
+import { useScroll, useTransform, motion, useMotionValueEvent, Variants } from 'framer-motion';
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
+
+const fadeInVariants: Variants = {
+  hidden: {
+    translateY: 0,
+  },
+  visible: {
+    width: '100%',
+    opacity: 1,
+    translateY: -10,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+const cardVariants: Variants = {
+  offscreen: {
+    opacity: 0,
+    y: 50,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0.6,
+      duration: 0.8,
+    },
+  },
+};
 
 export default function ServiceStart() {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    // 'target scrollposition'
-    offset: ['center start', 'start center'],
-  });
-  const opacity = useTransform(scrollYProgress, [0, 0.3, 0.7, 1], [0, 1, 1, 0]);
-  // opacity에 따라 disabled 상태를 결정
-
-  const [isClickable, setIsClickable] = useState(false);
-  useMotionValueEvent(scrollYProgress, 'change', (lastest) => {
-    lastest === 1 ? setIsClickable(false) : setIsClickable(true);
-  });
   return (
-    <div ref={ref} className='relative h-screen'>
-      <motion.div style={{ opacity }} className='fixed inset-0 flex flex-col justify-center items-center text-white '>
+    // amount 보이는 정도
+    <motion.div initial='offscreen' whileInView='onscreen' viewport={{ amount: 0.8 }} className='relative h-screen'>
+      <motion.div className='absolute h-[50%] bottom-0 '></motion.div>
+      <motion.div
+        variants={cardVariants}
+        className='fixed inset-0 flex flex-col justify-center items-center text-white '
+      >
         <h2 className='text-[43px] md:text-[86px] font-bold break-keep break-words text-center'>
           지금 바로 시작하세요
         </h2>
@@ -27,11 +47,10 @@ export default function ServiceStart() {
           whileHover={{ scale: 1.1 }}
           className='duration-150 mt-4 md:mt-8 py-1 px-4 md:py-2 md:px-8 text-lg md:text-2xl font-semibold rounded-[500px] bg-[#3cb643]'
           onClick={() => signIn()}
-          disabled={!isClickable}
         >
           서비스 이용하기
         </motion.button>
       </motion.div>
-    </div>
+    </motion.div>
   );
 }
